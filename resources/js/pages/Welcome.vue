116d0fdb0d08cb621ad541315ff56e03d5a8c75f
Paul-isol <isolpaul@gmail.com>
2026-03-09 21:15:39 +0530

    eslint unused component error fixed

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3';
-import { dashboard, login, myfiles, register } from '@/routes';
+import { login, myfiles, register } from '@/routes';
 
 withDefaults(
     defineProps<{
